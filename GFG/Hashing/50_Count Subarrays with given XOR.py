0aaf0850155b31dbeabc945d3cf276e3aec6a2f1
wtfashwin <ashwinupadhyay09@gmail.com>
2025-01-16 21:10:33 +0530

    ‎

---
+++ GFG/Hashing/50_Count Subarrays with given XOR.py
@@ -0,0 +1,14 @@
+'''
+Given an array of integers arr[] and a number k, the task is to count the number of subarrays having XOR of their elements as k.
+
+Examples: 
+
+Input: arr[] = [4, 2, 2, 6, 4], k = 6
+Output: 4
+Explanation: The subarrays having XOR of their elements as 6 are [4, 2], [4, 2, 2, 6, 4], [2, 2, 6], and [6].
+
+Input: arr[] = [5, 6, 7, 8, 9], k = 5
+Output: 2
+Explanation: The subarrays having XOR of their elements as 5 are [5] and [5, 6, 7, 8, 9].
+'''
+
