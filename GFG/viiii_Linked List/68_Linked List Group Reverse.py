c786fe88a57e447299d9205df5c4602f1577de88
wtfashwin <ashwinupadhyay09@gmail.com>
2025-01-21 22:59:07 +0530

    ‎

---
+++ GFG/viiii_Linked List/68_Linked List Group Reverse.py
@@ -0,0 +1,105 @@
+'''
+Given the head a linked list, the task is to reverse every k node in the linked list. If the number of nodes is not a multiple of k then the left-out nodes in the end, should be considered as a group and must be reversed.
+
+Examples:
+
+Input: head = 1 -> 2 -> 2 -> 4 -> 5 -> 6 -> 7 -> 8, k = 4
+Output: 4 -> 2 -> 2 -> 1 -> 8 -> 7 -> 6 -> 5
+
+Explanation: The first 4 elements 1, 2, 2, 4 are reversed first and then the next 4 elements 5, 6, 7, 8. Hence, the resultant linked list is 4 -> 2 -> 2 -> 1 -> 8 -> 7 -> 6 -> 5.
+Input: head = 1 -> 2 -> 3 -> 4 -> 5, k = 3
+Output: 3 -> 2 -> 1 -> 5 -> 4
+
+Explanation: The first 3 elements 1, 2, 3 are reversed first and then left out elements 4, 5 are reversed. Hence, the resultant linked list is 3 -> 2 -> 1 -> 5 -> 4.
+Constraints:
+1 <= size of linked list <= 105
+1 <= data of nodes <= 106
+1 <= k <= size of linked list 
+
+'''
+
+class Solution:
+
+    def reverseKGroup(self, head, k):
+        if head is None or k == 1:
+            return head
+
+        curr = head
+        newHead = None
+        tail = None
+
+        while curr is not None:
+            groupHead = curr
+            prev = None
+            nextNode = None
+            count = 0
+
+            # Reverse the nodes in the current group
+            while curr is not None and count < k:
+                nextNode = curr.next
+                curr.next = prev
+                prev = curr
+                curr = nextNode
+                count += 1
+
+            # If newHead is None, set it to the last node of the first group
+            if newHead is None:
+                newHead = prev
+
+            # Connect the previous group to the current reversed group
+            if tail is not None:
+                tail.next = prev
+
+            # Move tail to the end of the reversed group
+            tail = groupHead
+
+        return newHead
+
+
+
+#{ 
+ # Driver Code Starts
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def printList(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" ")
+            temp = temp.next
+        print()
+
+
+if __name__ == '__main__':
+    t = int(input())  # Number of test cases
+    while t > 0:
+        llist = LinkedList()
+
+        # Read list values and push them to the LinkedList
+        values = list(map(int, input().split()))
+        for i in reversed(values):
+            llist.push(i)
+
+        k = int(input())  # Size of the group for reversal
+        ob = Solution()
+        new_head = ob.reverseKGroup(llist.head, k)
+        llist.head = new_head
+        llist.printList()  # Print the modified linked list
+        t -= 1
+
+        print("~")
+
