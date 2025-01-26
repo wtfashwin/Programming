2597be10a5416358c849e59e01320d37bb0cbab2
wtfashwin <ashwinupadhyay09@gmail.com>
2025-01-26 17:28:49 +0530

    RT II

---
+++ Python With DSA/Coding pr 11 Right Angled Triangle II.py
@@ -0,0 +1,27 @@
+# Right Angled Triangle II
+# Problem Description:
+
+# You are given an integer n. Your task is to return a right-angled triangle pattern of '*', where each row contains stars aligned to the right. The first row has one star, the second row has two stars, and so on, until the nth row has n stars.
+
+
+
+# Input:
+
+# A single integer n, where 1 <= n <= 100.
+
+
+
+# Output:
+
+# A list of strings where each string represents a row in the right-angled triangle, right-aligned.
+
+
+
+# Example:
+
+# Input: 4
+# Output: ['   *', '  **', ' ***', '****']
+ 
+# Input: 3
+# Output: ['  *', ' **', '***']
+
